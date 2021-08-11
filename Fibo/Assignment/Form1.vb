572b1773bd8165ff74452a254b6c5200cb35f090
eltiiigre <60494494+eltiiigre@users.noreply.github.com>
2021-08-11 21:07:01 +0800

    Add files via upload

---
+++ Fibo/Assignment/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim num As Integer = Integer.Parse(TextBox1.Text)
+        Dim a As Integer = 0
+        Dim b As Integer = 1
+        Dim result As Integer
+        Dim s As String = "0 "
+
+        For asd = 1 To (num - 1)
+            s += b & " "
+            result = a + b
+            a = b
+            b = result
+        Next asd
+        TextBox2.Text = s
+
+    End Sub
+End Class
---
+++ Multiplication/Multiplication/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As Integer = Integer.Parse(TextBox1.Text)
+        Dim b As Integer = Integer.Parse(TextBox2.Text)
+        Dim c As Integer = 1
+        Dim ss As String = a & " x " & c & " = " & a * c & vbCrLf
+        Dim print As String
+        For asd = 1 To (b - 1)
+
+            c += 1
+            ss += a & " x " & c & " = " & a * c & vbCrLf
+        Next asd
+        MessageBox.Show(ss)
+    End Sub
+End Class
