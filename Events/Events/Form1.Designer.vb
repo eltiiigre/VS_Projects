fab54648b38790609e0e65221b0777b8dfba6fda
eltiiigre <60494494+eltiiigre@users.noreply.github.com>
2021-08-11 21:05:19 +0800

    Add files via upload

---
+++ Events/Events/Form1.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtAge = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(37, 59)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(63, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Customer Id"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(37, 98)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Name"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(37, 137)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(26, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Age"
+        '
+        'txtID
+        '
+        Me.txtID.Location = New System.Drawing.Point(135, 52)
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(100, 20)
+        Me.txtID.TabIndex = 3
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(135, 91)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(100, 20)
+        Me.txtName.TabIndex = 4
+        '
+        'txtAge
+        '
+        Me.txtAge.Location = New System.Drawing.Point(135, 130)
+        Me.txtAge.Name = "txtAge"
+        Me.txtAge.Size = New System.Drawing.Size(100, 20)
+        Me.txtAge.TabIndex = 5
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(91, 184)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Submit"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(299, 246)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtAge)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.txtID)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtAge As TextBox
+    Friend WithEvents Button1 As Button
+    Protected WithEvents txtID As TextBox
+End Class
---
+++ Events/Events/Form1.vb
@@ -0,0 +1,65 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+
+    Private Sub txtID_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtID.KeyPress, txtID.KeyPress
+        If Char.IsLetter(e.KeyChar) Then
+            e.Handled = True
+            MsgBox("Error!")
+        End If
+    End Sub
+
+    Private Sub txtName_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtName.KeyPress
+        If Char.IsNumber(e.KeyChar) Or Char.IsPunctuation(e.KeyChar) AndAlso Not e.KeyChar = "." AndAlso Not e.KeyChar = "-" Then
+            e.Handled = True
+            MsgBox("Error!")
+        End If
+    End Sub
+
+
+    Private Sub txtAge_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtAge.KeyPress
+
+        If Char.IsLetter(e.KeyChar) Or Char.IsPunctuation(e.KeyChar) Or txtAge.Text.Length > 2 Then
+            e.Handled = True
+            MsgBox("Error!")
+        End If
+    End Sub
+
+
+
+    Private Sub txtID_MouseEnter(sender As Object, e As EventArgs) Handles txtID.MouseEnter
+        txtID.BackColor = Color.Cyan
+        txtID.ForeColor = Color.Crimson
+    End Sub
+
+    Private Sub txtID_MouseLeave(sender As Object, e As EventArgs) Handles txtID.MouseLeave
+        txtID.BackColor = Color.White
+    End Sub
+
+    Private Sub txtName_MouseEnter(sender As Object, e As EventArgs) Handles txtName.MouseEnter
+        txtName.BackColor = Color.Cyan
+        txtName.ForeColor = Color.Crimson
+    End Sub
+
+    Private Sub txtName_MouseLeave(sender As Object, e As EventArgs) Handles txtName.MouseLeave
+        txtName.BackColor = Color.White
+    End Sub
+
+    Private Sub txtAge_MouseEnter(sender As Object, e As EventArgs) Handles txtAge.MouseEnter
+        txtAge.BackColor = Color.Cyan
+        txtAge.ForeColor = Color.Crimson
+    End Sub
+
+    Private Sub txtAge_MouseLeave(sender As Object, e As EventArgs) Handles txtAge.MouseLeave
+        txtAge.BackColor = Color.White
+    End Sub
+
+
+    Private Sub txtAge_KeyUp(sender As Object, e As KeyEventArgs) Handles txtAge.KeyUp
+        If Integer.Parse(txtAge.Text) > 122 Then
+            MsgBox("Errorrr")
+        End If
+    End Sub
+End Class
